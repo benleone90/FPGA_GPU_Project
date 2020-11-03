b4ff53a3b99638f30bacf6de451c8e5a6748fa01
Ben Leone <benleone90@gmail.com>
2020-11-02 19:07:53 -0500

    Finished neuron in C

--- Sprint_3/neuron.c
+++ Sprint_3/neuron.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 float sigmoid(float x)
@@ -15,7 +16,33 @@ float sigmoid(float x)
     return return_value;
 }
 
+float dot_product(int *u, int *v)
+{
+    float sum = 0;
+    int n = sizeof(u) / sizeof(u[0]);
+    for (size_t i = 0; i < n; i++)
+    {
+        sum += u[i] * v[i];
+    }
+    return sum;
+}
+
+float feedforward(int *neuron, int *input, int bias)
+{
+    float total = dot_product(neuron, input) + bias;
+    return sigmoid(total);
+}
+
+struct neuron
+{
+    int weights[2];
+    int bias;
+};
+
 int main()
 {
-    printf("%.6f\n", sigmoid(-6));
+    struct neuron n1 = {{0, 1}, 4}; // w1 = 0, w2 = 1, b = 4
+    int inputs[2] = {2, 3};         // x1 = 2, x2 = 3
+    printf("%.20f\n", feedforward(n1.weights, inputs, n1.bias));
+    return EXIT_SUCCESS;
 }
